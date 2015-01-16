0a24c5b146dd72e6615946a343c8752a412ce0a5
MatthewRuddy <info@matthewruddy.com>
2015-01-16 22:34:26 +0000

    Added alt text attribute to images, which was missing previously.

--- includes/class-es-slider.php
+++ includes/class-es-slider.php
@@ -664,7 +664,7 @@ class ES_Slider {
 		$image_url = apply_filters( 'easingslider_modify_image_url', $image_url, $slider->dimensions->width, $slider->dimensions->height );
 
 		// Add the HTML
-		$html .= "<img src=\"{$image_url}\" title=\"{$slide->title}\" class=\"easingslider-image\" />";
+		$html .= "<img src=\"{$image_url}\" title=\"{$slide->title}\" alt=\"{$slide->alt}\" class=\"easingslider-image\" />";
 
 		return $html;
 
